02acedc0f98a074640c39bcd03fa7e14eb9a25c0
LNT-21 <lnt-21@ankit-macmini.local>
2016-04-28 11:05:15 +0530

    Initial Commit

---
+++ RecordingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordingDemo
+//
+//  Created by LNT-21 on 28/04/16.
+//  Copyright © 2016 LNT-21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
